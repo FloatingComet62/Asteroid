2d3c12f11355625a0059a4ed23ceed67f0e21514
FloatingComet62 <shubh2007rai@gmail.com>
2022-09-11 20:11:32 +0530

    Stats command

---
+++ src/commands/stats.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder, EmbedBuilder } from '@discordjs/builders'
+import { Interactions, otherOptions } from '../interfaces'
+
+export const data = new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('See your stats');
+
+export async function execute({ database }: otherOptions, interaction: Interactions){
+    const { c, cpp, java, rust, javascript: js, typescript: ts, python: py, general: gen, "c-sharp": cs } = await database.getUser(interaction.user.id)
+
+    const embed = new EmbedBuilder()
+        .setTitle('Stats')
+        .setDescription(
+            `
+            **General**:    \`${gen}\`
+            **C**:          \`${c}\`
+            **Cpp**:        \`${cpp}\`
+            **C Sharp**:    \`${cs}\`
+            **Java**:       \`${java}\`
+            **Rust**:       \`${rust}\`
+            **JavaScript**: \`${js}\`
+            **TypeScript**: \`${ts}\`
+            **Python**:     \`${py}\``
+        )
+        .setColor(0xaaaaaa);
+    
+    interaction.reply({ embeds: [embed.toJSON()], ephemeral: true })
+}
